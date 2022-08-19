f35bfcf96101bea16a5810522bf8efd0023fd81e
roub0002 <gael.roubahie-fissa@etudiant.univ-reims.fr>
2022-08-19 22:35:02 +0200

    Initial commit

---
+++ clippy/target/debug/deps/clippy3-a1af4252783b70ad.d
@@ -0,0 +1,7 @@
+C:\Users\gaelr\Desktop\Rust_stuff\rustlings\exercises\clippy\target\debug\deps\clippy3-a1af4252783b70ad.rmeta: clippy3.rs
+
+C:\Users\gaelr\Desktop\Rust_stuff\rustlings\exercises\clippy\target\debug\deps\clippy3-a1af4252783b70ad.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
